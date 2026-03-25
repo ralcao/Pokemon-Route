b837a0a751a894b00b8cb0bff7f27b869a05c949
Rui <ruicoelho2511@gmail.com>
2026-03-25 14:10:58 +0000

    Fase 1 - Prolog

--- pokemon_route_students/prolog/pokemon_game.pl
+++
@@ -1,9 +0,0 @@
-:-ensure_loaded("pokemon_list.pl").
-:-ensure_loaded("pokemon_info_attacks.pl").
-:-ensure_loaded("pokemon_route.pl").
-
-player_starts(0,0).
-
-% TO DO
-next_rooms(X,Y,Rooms) :-
-    route(M).
